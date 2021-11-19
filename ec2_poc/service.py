e755321259316d77cebf15ef35fdfec78ec08105
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-11-18 21:37:49 -0500

    Update service.py

--- ec2_poc/service.py
+++ ec2_poc/service.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+import os
+import pickle
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
+def index():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/predict", methods=["POST", ])
+def predict():
+    print(request.form)
+    print(dict(request.form))
+    return "good"
+    # here = os.path.dirname(os.path.abspath(__file__))
+    # path_model_dump = os.path.join(here, "model.pk")
+    # with open(path_model_dump, "rb") as f:
+    #     classifier = pickle.load(f)
