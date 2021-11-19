18644b6b2dfdfcead1140f35fb19bb3d8ddf979c
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-11-18 21:31:58 -0500

    Update train_model.py

--- ec2_poc/train_model.py
+++ ec2_poc/train_model.py
@@ -6,6 +6,8 @@ Reference:
 https://scikit-learn.org/stable/auto_examples/classification/plot_classification_probability.html#sphx-glr-auto-examples-classification-plot-classification-probability-py
 """
 
+import os
+import pickle
 import numpy as np
 
 from sklearn.metrics import accuracy_score
@@ -13,21 +15,33 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.gaussian_process.kernels import RBF
 from sklearn import datasets
 
-iris = datasets.load_iris()
-X = iris.data[:, 0:2]  # we only take the first two features for visualization
-y = iris.target
 
-n_features = X.shape[1]
+def train_model():
+    iris = datasets.load_iris()
+    X = iris.data[:, 0:2]  # we only take the first two features for visualization
+    y = iris.target
 
-C = 10
-kernel = 1.0 * RBF([1.0, 1.0])  # for GPC
+    C = 10
 
-classifier = LogisticRegression(
-    C=C, penalty="l1", solver="saga", multi_class="multinomial", max_iter=10000
-)
+    classifier = LogisticRegression(
+        C=C, penalty="l1", solver="saga", multi_class="multinomial", max_iter=10000
+    )
 
-classifier.fit(X, y)
+    classifier.fit(X, y)
 
-y_pred = classifier.predict(X)
-print(X)
-print(y_pred)
+    return classifier
+
+
+def train_and_dump_model():
+    here = os.path.dirname(os.path.abspath(__file__))
+    path_model_dump = os.path.join(here, "model.pk")
+    if os.path.exists(path_model_dump):
+        return
+    classifier = train_model()
+
+    with open(path_model_dump, "wb") as f:
+        pickle.dump(classifier, f)
+
+
+if __name__ == "__main__":
+    train_model()
