a3f9f821a4e842b254e7ef3c7a026ffd8b4f04da
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-11-18 21:41:17 -0500

    Update service.py

--- ec2_poc/service.py
+++ ec2_poc/service.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import json
 import pickle
 from flask import Flask, request
 
@@ -14,10 +15,11 @@ def index():
 
 @app.route("/predict", methods=["POST", ])
 def predict():
-    print(request.form)
-    print(dict(request.form))
-    return "good"
-    # here = os.path.dirname(os.path.abspath(__file__))
-    # path_model_dump = os.path.join(here, "model.pk")
-    # with open(path_model_dump, "rb") as f:
-    #     classifier = pickle.load(f)
+    data = json.loads(request.form["data"])
+    here = os.path.dirname(os.path.abspath(__file__))
+    path_model_dump = os.path.join(here, "model.pk")
+    with open(path_model_dump, "rb") as f:
+        classifier = pickle.load(f)
+    pred = classifier.predict(data)
+    response = {"pred": pred}
+    return json.dumps(response)
