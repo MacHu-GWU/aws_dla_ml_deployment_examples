2752450e2f0587ca66604aa53483fb023d8db248
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-11-18 21:26:02 -0500

    commit

---
+++ ec2_poc/README.rst
@@ -0,0 +1,16 @@
+Deploy ML model to EC2
+==============================================================================
+
+Just for POC
+
+
+
+.. code-block:: bash
+
+    # create virtualenv
+    virtualenv -p python3.8 venv
+
+    # enter virtualenv
+    source ./venv/bin/activate
+
+
---
+++ ec2_poc/train_model.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+Reference:
+
+https://scikit-learn.org/stable/auto_examples/classification/plot_classification_probability.html#sphx-glr-auto-examples-classification-plot-classification-probability-py
+"""
+
+import numpy as np
+
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.gaussian_process.kernels import RBF
+from sklearn import datasets
+
+iris = datasets.load_iris()
+X = iris.data[:, 0:2]  # we only take the first two features for visualization
+y = iris.target
+
+n_features = X.shape[1]
+
+C = 10
+kernel = 1.0 * RBF([1.0, 1.0])  # for GPC
+
+classifier = LogisticRegression(
+    C=C, penalty="l1", solver="saga", multi_class="multinomial", max_iter=10000
+)
+
+classifier.fit(X, y)
+
+y_pred = classifier.predict(X)
