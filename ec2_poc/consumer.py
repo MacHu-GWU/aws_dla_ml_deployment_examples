0c7d476fceb1d00f1053a6207263eb0b847d38ed
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-11-18 21:39:27 -0500

    Update consumer.py

--- ec2_poc/consumer.py
+++ ec2_poc/consumer.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import json
 import requests
 
 res = requests.get("http://127.0.0.1:5000")
+print(res.text)
+
+data = [
+    [6., 3.4],
+    [6.7, 3.1],
+    [6.3, 2.3],
+]
+res = requests.post("http://127.0.0.1:5000/predict", data={"data": json.dumps(data)})
 print(res.text)
