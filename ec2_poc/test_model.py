9ea14e4b3270cbac88b0827e009b226268cf1940
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-11-18 21:34:32 -0500

    Update test_model.py

--- ec2_poc/test_model.py
+++ ec2_poc/test_model.py
@@ -3,18 +3,17 @@
 import os
 import pickle
 
-
 here = os.path.dirname(os.path.abspath(__file__))
 path_model_dump = os.path.join(here, "model.pk")
 
 with open(path_model_dump, "rb") as f:
     classifier = pickle.load(f)
 
-
 X = [
-    [6.,  3.4],
+    [6., 3.4],
     [6.7, 3.1],
     [6.3, 2.3],
 ]
 
 pred = classifier.predict(X)
+print(pred)
